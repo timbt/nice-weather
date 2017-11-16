b04a530df0cf339769b5437b4f0a81063a5bc250
Timothy Beattie <timothy.beattie@canada.ca>
2017-11-16 20:58:11 +0000

    Return provided city name as document text element instead of pop-up.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,30 @@
+function renderNewResponse(cityName){
+    var responseContainer = document.createElement("div");
+    responseContainer.setAttribute('id', 'response-container');
+    var content = document.createTextNode("City: " + cityName);
+    //content.setAttribute('id', 'city-name-response');
+    responseContainer.appendChild(content);
+    var responseArea = document.getElementById("response-area");
+    responseArea.appendChild(responseContainer);
+}
+
+function reRenderResponse(cityName){
+    var responseContainer = document.getElementById('response-container');
+    responseContainer.remove();
+    renderNewResponse(cityName);
+}
+
+function renderResponse(cityName){
+    var responseContainer = document.getElementById('response-container');
+    if (!responseContainer) {
+        renderNewResponse(cityName);
+    } 
+    else {
+        reRenderResponse(cityName);
+    } 
+}
+
 function handleForm(){
     var cityName = document.getElementById('city-name').value;
-    alert('You entered: ' + cityName + '.');
-    return false;
+    renderResponse(cityName);
 }
